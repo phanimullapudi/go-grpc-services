37e8745e55c04aa21c257bf550bb913c6335f402
Phani Mullapudi <phani.mullapudi@gmail.com>
2022-01-05 21:04:33 -0600

    first commit

---
+++ cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "log"
+
+func Run() error {
+
+	// responsible for intializing and starting
+	// our gRPC server
+	return nil
+}
+
+func main() {
+
+	if err := Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
